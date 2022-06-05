4ddefab4939cb0518850b11da5c0ad8a358ac527
EC2 Default User <ec2-user@ip-172-31-84-145.ec2.internal>
2022-06-05 12:45:01 +0000

    Initial Commit1

---
+++ app/controllers/hauses_controller.rb
@@ -0,0 +1,2 @@
+class HausesController < ApplicationController
+end
---
+++ test/controllers/hauses_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HausesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
